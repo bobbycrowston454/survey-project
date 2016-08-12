fb344085637479cf099be63588a9750175ebd01e
bobbycrowston <bobbycrowston454@gmail.com>
2016-08-12 08:37:29 -0700

    add survey questions and working javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
-  $(".form").submit(function(event) {
+  $(".course").submit(function(event) {
     var type = $("#type1").val();
-    var experience1 = $("#experience").val();
-    var experience1 = $("#experience").val();
-    if () {
+    var experience = $("#experience").val();
+    var interest = $("#type1").val();
+    var nameinput = $("input#name1").val();
 
+    $(".fpath").hide
+    $(".mpath").hide
+    $(".bpath").hide
+    
+
+    $(".name").text(nameinput);
+    if (interest === "mobile" && experience === "some") {
+      $(".mpath").show();
+    } else if (interest === "front" && experience ==="none") {
+      $(".fpath").show();
     }
+    event.preventDefault();
   });
 });
