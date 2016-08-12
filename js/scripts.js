cd76f98d27d62ee9147df71b19e6d9588365f9f1
bobbycrowston <bobbycrowston454@gmail.com>
2016-08-11 22:37:50 -0700

    add some form boxes and beginning of javascript

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(".form").submit(function(event) {
+    var type = $("#type1").val();
+    var experience1 = $("#experience").val();
+    var experience1 = $("#experience").val();
+    if () {
+
+    }
+  });
+});
