15508cd0ce1c70fb9121f77fc6c1bbb77afd8460
bobbycrowston <bobbycrowston454@gmail.com>
2016-08-12 10:59:18 -0700

    add another dropdown form added, survey working, styling in progress

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,26 @@ $(document).ready(function() {
     var interest1 = $("#interest1").val();
     var experience = $("#experience").val();
     var company1 = $("#company1").val();
+    var style1 = $("#style1").val();
     var nameinput = $("input#name1").val();
 
     $(".fpath").hide();
     $(".bpath").hide();
     $(".mpath").hide();
     $(".cpath").hide();
+    $(".dpath").hide();
 
     $(".name").text(nameinput);
     if (interest1 === "mobile" && experience === "some" && company1 === "small" || interest1 === "mobile" && experience === "none" || interest1 === "mobile" && company1 === "small" ) {
       $(".mpath").show();
     } else if (interest1 === "front" && experience === "none" && company1 === "small" || company1 === "big" && interest1 === "front" && experience === "some" || company1 === "big" && interest1 === "front" && experience === "none") {
       $(".fpath").show();
-    } else if (interest1 === "back" && experience === "some" && company1 === "small" || interest1 === "back" && experience === "none" && company1 === "small" ) {
+    } else if (interest1 === "back" && experience === "some" && company1 === "small" && style1 === "no"|| interest1 === "back" && experience === "none" && company1 === "small" && style1 === "no") {
       $(".bpath").show();
-    } else if (interest1 === "back" && experience === "some" && company1 === "big" || interest1 === "back" && experience === "none" && company1 === "big" ) {
+    } else if (interest1 === "back" && experience === "some" && company1 === "big" && style1 === "no" || interest1 === "back" && experience === "none" && company1 === "big" && style1 === "no") {
       $(".cpath").show();
+    } else if (interest1 === "back" && experience === "some" && company1 === "small" && style1 ==="yes"|| interest1 === "back" && experience === "none" && company1 === "small" && style1 === "yes") {
+      $(".dpath").show();
     }
     event.preventDefault();
   });
