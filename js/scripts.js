f032acdf9b188edbb3c7534147a69a55dd9415a6
bobbycrowston <bobbycrowston454@gmail.com>
2016-08-12 10:17:17 -0700

    finish working forms and begin styling of the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,24 @@
 $(document).ready(function() {
   $(".course").submit(function(event) {
-    var type = $("#type1").val();
+    var interest1 = $("#interest1").val();
     var experience = $("#experience").val();
-    var interest = $("#type1").val();
+    var company1 = $("#company1").val();
     var nameinput = $("input#name1").val();
 
-    $(".fpath").hide
-    $(".mpath").hide
-    $(".bpath").hide
-    
+    $(".fpath").hide();
+    $(".bpath").hide();
+    $(".mpath").hide();
+    $(".cpath").hide();
 
     $(".name").text(nameinput);
-    if (interest === "mobile" && experience === "some") {
+    if (interest1 === "mobile" && experience === "some" && company1 === "small" || interest1 === "mobile" && experience === "none" || interest1 === "mobile" && company1 === "small" ) {
       $(".mpath").show();
-    } else if (interest === "front" && experience ==="none") {
+    } else if (interest1 === "front" && experience === "none" && company1 === "small" || company1 === "big" && interest1 === "front" && experience === "some" || company1 === "big" && interest1 === "front" && experience === "none") {
       $(".fpath").show();
+    } else if (interest1 === "back" && experience === "some" && company1 === "small" || interest1 === "back" && experience === "none" && company1 === "small" ) {
+      $(".bpath").show();
+    } else if (interest1 === "back" && experience === "some" && company1 === "big" || interest1 === "back" && experience === "none" && company1 === "big" ) {
+      $(".cpath").show();
     }
     event.preventDefault();
   });
